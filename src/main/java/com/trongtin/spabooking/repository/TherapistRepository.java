7c081d0476157b686c46c936dff5558fc31aa719
Thai Trong Tin <tintt362@gmail.com>
2026-02-13 18:00:54 +0700

    feature: impl therapist repo

---
+++ src/main/java/com/trongtin/spabooking/repository/TherapistRepository.java
@@ -0,0 +1,21 @@
+package com.trongtin.spabooking.repository;
+
+import com.trongtin.spabooking.entity.Therapist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TherapistRepository extends JpaRepository<Therapist, Long> {
+
+    // kiem tat ca Therapist active = true
+    List<Therapist> findByIsActiveTrue();
+
+    // kiem tat ca Employee voi code
+    Optional<Therapist> findByEmployeeCode(String code);
+
+    // kiem tat ca Therapist bang email
+    Optional<Therapist> findByEmail(String email);
+}
