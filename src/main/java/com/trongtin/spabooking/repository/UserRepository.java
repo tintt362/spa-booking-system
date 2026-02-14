48c44896300f0681097c8c7f07cb751344a39f27
Thai Trong Tin <tintt362@gmail.com>
2026-02-14 17:06:25 +0700

    impl user repo

---
+++ src/main/java/com/trongtin/spabooking/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.trongtin.spabooking.repository;
+
+import com.trongtin.spabooking.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByPhone(String phone);
+
+    boolean existsByEmail(String email);
+
+    boolean existByPhone(String phone);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.deletedAt IS NULL")
+    Optional<User> findActiveByEmail(String email);
+}
