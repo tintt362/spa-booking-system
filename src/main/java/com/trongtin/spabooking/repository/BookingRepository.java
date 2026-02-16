65af779b68caf53f44652b0b44ca76d0ae64c61b
Thai Trong Tin <tintt362@gmail.com>
2026-02-16 11:15:28 +0700

    feat: booking - service controller

--- src/main/java/com/trongtin/spabooking/repository/BookingRepository.java
+++ src/main/java/com/trongtin/spabooking/repository/BookingRepository.java
@@ -3,11 +3,11 @@ package com.trongtin.spabooking.repository;
 import com.trongtin.spabooking.entity.Booking;
 import com.trongtin.spabooking.entity.BookingStatus;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/trongtin/spabooking/repository/UserRepository.java
+++ src/main/java/com/trongtin/spabooking/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.trongtin.spabooking.repository;
 
 import com.trongtin.spabooking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
